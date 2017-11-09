dd43277d918eae13afa7005ee5e47f59f0fce0be
kristiankyvik <kristianruizkyvik@gmail.com>
2017-11-09 15:48:14 +0100

    added navbar in review page

--- pages/review.js
+++ pages/review.js
@@ -5,21 +5,23 @@ import Footer from '../components/Footer'
 
 export default (props) => (
   <App>
-   <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src={`https://kyvikbcn.typeform.com/to/FsuJQm?id=${props.url.query ? props.url.query.id : ""}`}></iframe> 
-   <style jsx>{`
-    html{ 
-      margin: 0;
-      height: 100%;
-      overflow: hidden;
-    } 
-    iframe{
-      position: absolute;
-      left:0;
-      right:0;
-      bottom:0;
-      top:0;
-      border:0;
-    }
-   `}</style>
+    <Header/>
+    <Hero pathname={props.url.pathname} />
+    <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src={`https://kyvikbcn.typeform.com/to/FsuJQm?id=${props.url.query ? props.url.query.id : ""}`}></iframe> 
+    <style jsx>{`
+      html{ 
+        margin: 0;
+        height: 100%;
+        overflow: hidden;
+      } 
+      iframe{
+        position: absolute;
+        left:0;
+        right:0;
+        bottom:0;
+        top:70px;
+        border:0;
+      }
+    `}</style>
   </App>
 )
