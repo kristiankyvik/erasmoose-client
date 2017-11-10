8b62f93a863a56a4b1839cf39b0047f741d5c586
pvp23 <patrick.v.platen@gmail.com>
2017-11-10 13:25:02 +0100

    forgot index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Header from '../components/Header'
 import Submit from '../components/Submit'
 import Hero from '../components/Hero'
 import UniList from '../components/UniList'
-import withData from '../lib/withData'
 import Footer from '../components/Footer'
 
 export default (props) => (
