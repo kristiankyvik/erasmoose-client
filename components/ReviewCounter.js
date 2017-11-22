be7a8c94d47c61162790c335417aa1dad61f1e4e
kristiankyvik <kristianruizkyvik@gmail.com>
2017-11-22 09:00:42 +0100

    fixed query

--- components/ReviewCounter.js
+++ components/ReviewCounter.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { gql, graphql } from 'react-apollo'
 
 function countReviews(data) {
-  console.log("Look at the data object. It returns allReviews with empty properties. If you take the aggregate query for allReviews and try it out in robo 3T you see that the query is correct. Please verify what's wrong there. The solution that is implemented now works but I use map and reduce which should be done in mongo.", data)
-  
-   const unisReviewed = data.allUnisWithReview.length
-   const reviews = data.allUnisWithReview.map(elm => elm.reviews_count).reduce((agg, cur) => (agg + cur))
-  
   return (
     <div className="flex pr4 flex-column pl5">
       <div className="flex items-center f6 b pb1">
@@ -16,13 +11,13 @@ function countReviews(data) {
         <div className="tl">
           <span className="b f6">
             <i className="fa fa-university mr1" aria-hidden="true"></i>
-            { unisReviewed } unis
+            { data._allReviewsMeta.unisCount } unis
           </span>
         </div>
         <div className="tl">
           <span className="b f6">
             <i className="fa fa-user mr2" aria-hidden="true"></i>
-            { reviews } reviews
+            { data._allReviewsMeta.reviewsCount } reviews
           </span>
         </div>
       </div>
@@ -32,12 +27,9 @@ function countReviews(data) {
 
 const getUnisWithReview = gql`
   query {
-    allUnisWithReview {
-      reviews_count
-    }
-    allReviews {
-      allReviews
-      allUnisReviewed
+    _allReviewsMeta {
+      unisCount
+      reviewsCount
     }
   }
 `
