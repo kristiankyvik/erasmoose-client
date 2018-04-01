3450af5d2e2b35be3e74fc7e37a32bb83fa49963
pvp23 <patrick.v.platen@gmail.com>
2018-04-02 00:25:19 +0200

    small change

--- components/Modal.js
+++ components/Modal.js
@@ -79,7 +79,7 @@ class Modal extends React.Component {
 
 	getWeatherForecast(city) {
 		var xhr = new XMLHttpRequest();
-		const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=796d6ae51449b3531c9a4df53a4f6413";
+		const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=796d6ae51449b3531c9a4df53a4f6413";
 		xhr.open("GET", url , true);
 		xhr.send();
 		
